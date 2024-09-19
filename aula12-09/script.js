dfc665bbdd009d392ac1915727e060a86dbb9772
Sergio Cunha Lima Bezerra <sergiocunha50@gmail.com>
2024-09-19 17:23:33 -0300

    add: exercico dia 19/09

---
+++ aula12-09/script.js
@@ -0,0 +1,26 @@
+let livros = [
+    {"titulo": "Um aaray no pedaço1", "qunatidade": "8"},
+    {"titulo": "Um aaray no pedaço2", "qunatidade": "9"},
+    {"titulo": "Um aaray no pedaço3", "qunatidade": "1"},
+    {"titulo": "Um aaray no pedaço4", "qunatidade": "10"},
+    {"titulo": "Um aaray no pedaço1", "qunatidade": "9"}
+]
+function livrosComEstoqueSuficiente(livros){
+    // let resultado = [];
+    // for let = 0; i < livros.length(livros); i++){
+    //     if (livros[i].qunatidade >= 10){
+    //         resultado.push(livros[i]);
+    //     }
+    // }
+    livros.forEach(livros => {
+        if(livros.qunatidade >= 10){
+            resultado.push(livros)
+        }    
+    };
+    if (resultado.length > 0){
+        return resultado;
+    } else {
+        return "Nenhum livro com quantidade maior ou igual a 10"
+    }
+}
+console.log(livrosComEstoqueSuficiente(livros));
---
+++ aula19-09/script.js
@@ -0,0 +1,27 @@
+function quickSort(arr) {
+    if (arr.length <= 1) {
+      return arr; // Base case: array with 0 or 1 element is already sorted
+    }
+  
+    const pivot = arr[arr.length   
+   - 1]; // Choose the last element as the pivot
+    const left = [];
+    const right = [];
+  
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] < pivot) {
+        left.push(arr[i]);   
+  
+      } else {
+        right.push(arr[i]);
+      }
+    }
+  
+    return quickSort(left).concat(pivot, quickSort(right));
+  }
+  
+  // Example usage:   
+  
+  const unsortedArray = [5, 2, 9, 1, 7, 3];
+  const sortedArray = quickSort(unsortedArray);
+  console.log(sortedArray); // Output: [1, 2, 3, 5, 7, 9]
